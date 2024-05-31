6ff0d7057c66ebdc7dffb433d606bbf536a923d4
Ahmed Elmaadawy <ahmed.elmaadawy03@gmail.com>
2024-05-31 12:02:40 +0300

    Complete Cart RazorPage

---
+++ SportsStore-MVC/Infrastructure/SessionExtentions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace SportsStore_MVC.Infrastructure
+{
+    /// <summary>
+    /// These Methods seralize objects into JSON Format to make it ease to store and retrieve cart objects
+    /// </summary>
+    public static class SessionExtentions
+    {
+        public static void SetJson(this ISession session,string key,object value)
+        {
+            session.SetString(key,JsonSerializer.Serialize(value));
+        }
+        public static T? GetJson<T>(this ISession session, string key) { 
+            var sessionData =session.GetString(key);
+            return sessionData == null ?
+                default(T) :JsonSerializer.Deserialize<T>(sessionData);
+        }
+    }
+}
--- SportsStore-MVC/Pages/Cart.cshtml.cs
+++ SportsStore-MVC/Pages/Cart.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SportsStore_MVC.Infrastructure;
+using SportsStore_MVC.Models;
+using SportsStore_MVC.Repository;
 
 namespace SportsStore_MVC.Pages
 {
     public class CartModel : PageModel
     {
-        public void OnGet()
+        private IStoreRepository repository;
+        public CartModel(IStoreRepository repository)
         {
+            this.repository = repository;
+        }
+        public Cart? Cart { get; set; }
+        public string ReturnUrl { get; set; } = "/";
+
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+            Cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+
+        }
+        public IActionResult OnPost(long productId, string returnUrl)
+        {
+            Product? product = repository.Products.FirstOrDefault(p => p.ProductId == productId);
+            if (product != null)
+            {
+                Cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+                Cart.AddItem(product, 1);
+                HttpContext.Session.SetJson("Cart", Cart);
+
+            }
+            return RedirectToPage(new { returnUrl = returnUrl });
         }
     }
 }
